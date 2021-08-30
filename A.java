01afa29d6f485aea088886793e9ad0ead416ef9f
analu19 <65090369+analu19@users.noreply.github.com>
2021-08-29 21:19:18 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,25 @@
+public class A {
+    int A1;
+    float A2;
+
+    int getA1(){
+        return A1;
+    }
+
+    float getA2(){
+        return A2;
+    }
+
+    void setA(int num1, float num2){
+        A1 = num1;
+        A2 = num2;
+    }
+
+    void MA1(){
+        System.out.println("MA1");
+    }
+
+    void MA2(){
+        System.out.println("MA2");
+    }
+}
---
+++ B.java
@@ -0,0 +1,26 @@
+public class B {
+    int B1;
+    float B2;
+
+    int getB1(){
+        return B1;
+    }
+
+    float getB2(){
+        return B2;
+    }
+
+    void setB(int num1, float num2){
+        B1 = num1;
+        B2 = num2;
+    }
+    void MB1(){
+        System.out.println("MB1");
+    }
+
+    void MB2(){
+        System.out.println("MB2");
+    }
+
+}
+
