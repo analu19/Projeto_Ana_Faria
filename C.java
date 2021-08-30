746eecc45d56ae12a00af968cdfc564d618229dc
Ana Luiza <ana.calixto@estudante.com>
2021-08-29 22:34:20 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,25 @@
+public class C {
+    int C1;
+    float C2;
+
+    int getC1(){
+        return C1;
+    }
+
+    float getC2(){
+        return C2;
+    }
+
+    void setC(int num1, float num2){
+        C1 = num1;
+        C2 = num2;
+    }
+
+    void MC1(){
+        System.out.println("MC1");
+    }
+
+    void MC2(){
+        System.out.println("MC2");
+    }
+}
