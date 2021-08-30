93ef6017b5e1cc63ce0acca6263083c0cc01b045
Ana Luiza <ana.calixto@estudante.com>
2021-08-29 22:58:41 -0300

    Criando classe D

---
+++ D.java
@@ -0,0 +1,28 @@
+public class D(){
+    
+    int D1;
+    float D2;
+
+      int getD1(){
+        return D1;
+    }
+
+    float getD2(){
+        return D2;
+    }
+
+    void setD(int num1, float num2){
+        D1 = num1;
+        D2 = num2;
+    }
+
+    void MD1(){
+        System.out.println("MD1");
+    }
+
+    void MD2(){
+        System.out.println("MD2");
+    }
+
+
+}
